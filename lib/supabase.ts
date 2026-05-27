609f6a01b8f1751775627810213e474ca44ba823
WatchingFromAfar23 <laurastadler2310@gmail.com>
2026-05-28 01:58:54 +0200

    update supabase key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,10 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 import { Platform } from 'react-native';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = 'https://upacfrnbpuclkbsumkgj.supabase.co';
+const supabaseUrl = 'https://upacfrnbpuclkbsumkgj.supabase.co/rest/v1/';
 
 const supabaseAnonKey =
-  'YOUR_ANON_KEY';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwYWNmcm5icHVjbGtic3Vta2dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4OTU4MzgsImV4cCI6MjA5NTQ3MTgzOH0.bBlNMNiGm-J3zPeXs908vZPiw2AFflxALYK_FaOHh6c';
 
 export const supabase = createClient(
   supabaseUrl,
