35b95827ebc00b1d934001a66dd25a4f05617387
WatchingFromAfar23 <laurastadler2310@gmail.com>
2026-05-28 01:31:58 +0200

    fix web export

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,14 +1,25 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native';
 import 'react-native-url-polyfill/auto';
 
 const supabaseUrl = 'https://upacfrnbpuclkbsumkgj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwYWNmcm5icHVjbGtic3Vta2dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4OTU4MzgsImV4cCI6MjA5NTQ3MTgzOH0.bBlNMNiGm-J3zPeXs908vZPiw2AFflxALYK_FaOHh6c';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage: AsyncStorage,
-    detectSessionInUrl: false,
-    persistSession: true,
-  },
-});
+const supabaseAnonKey =
+  'YOUR_ANON_KEY';
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey,
+  {
+    auth: {
+      storage: Platform.OS === 'web'
+        ? undefined
+        : AsyncStorage,
+
+      detectSessionInUrl: false,
+      persistSession: true,
+      autoRefreshToken: true,
+    },
+  }
+);
